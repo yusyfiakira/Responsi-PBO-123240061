9f5161e40ed30c3397862c7e7f6b8d52ee3c1b99
zen | yusyfi <141817824+yusyfiakira@users.noreply.github.com>
2026-05-21 12:22:52 +0700

    Add files via upload

---
+++ Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+//123240061_Yusyfi Akira Arlyn Alzena
+import View.Karyawan.ViewData;
+import Controller.DatabaseConnection;
+
+public class Main {
+    public static void main(String[] args) {
+        new ViewData();
+    }
+}
